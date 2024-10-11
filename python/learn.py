cfe5eff85e2a0e5d662d1b7aa5bfb1fbf941f1b0
mohammedkashimawo <57919380+mohammedkashimawo@users.noreply.github.com>
2024-10-11 03:04:27 -0700

    Add files via upload

---
+++ python/learn.py
@@ -0,0 +1,70 @@
+if 5<10:
+    print("try to know how indentaton works ")#this is an emple of what comment looks like
+    print("can you now see that all the execution statements are ll on one line ")
+    
+    """
+    you can use this for your multiline comment
+    
+    """
+    
+    
+    student="mohammed mojeed ramadan"
+    course ='MSC  Arthificial Intelligence'
+    
+    
+    x=20
+    print(x)
+    
+    
+    y=int(41916254)
+    print(type(y))
+    
+    x,y,z ="food","water",'house'
+    print(z)
+    
+    
+    lam=34
+    girls=["shade",'tope','tayo','tade']
+    first,second,third,fourth =girls #this process is called unpacking collection
+    print(first + second)
+    print(first,second,third,fourth) 
+    #print(lam+first) #this will definitely return an error.
+    
+    
+    x=34
+    x=float(34)  
+    
+    print(x)
+    
+    
+    
+    import random
+    print(random.randrange(100,3454))\
+        
+        #list
+        
+        
+    thislist =['banaba','lesson','joy','office','lesson']
+    print(len(thislist))
+    
+    
+    code= list(('noises','ate'))
+    print(code)
+    
+    
+    
+    pri = ['john','tayo','tolu',404,]
+    for x in pri:
+      print(pri[0])
+      
+      
+    for a in range(len(pri)):
+        print(pri[a])
+        
+        
+social = ['facebook','instagram','twitter']
+
+i=0
+while i< len(social):
+    print(social[i])
+    i=i+1
